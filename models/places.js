64df57d07221840e6c37d07ccbdc9bc211cb1603
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-05-21 11:11:02 -0500

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: '',
+    pic: '/images/img.jpeg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: '',
+    pic: '/images/img2_2.jpeg'
+  }]
+  
