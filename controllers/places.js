ecf18710a8da5d98ea4896893f3c8cb758824ef1
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-05-22 14:35:05 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -12,6 +12,35 @@ router.get('/new', (req, res) => {
   })
   
   
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id], id })
+    }
+  })
+  
+  router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+
+  
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const placesRoutes = require('./controllers/places')
 const app = express()
+const methodOverride = require('method-override')
+
+
 // Express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 // Routes
 app.use('/places', placesRoutes)
