155b8cdb0ba7f40c6e7a1cab2a15ef3e0f6531bd
Rommel Gouala <rommelgouala@MacBook-Pro.lan>
2022-05-22 13:43:29 -0500

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,6 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: '',
-    pic: '/images/img.jpeg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: '',
-    pic: '/images/img2_2.jpeg'
-  }]
-  
 
 
 router.get('/', (req, res) => {
@@ -22,6 +9,23 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('places/new')
+  })
+  
+  
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
 
